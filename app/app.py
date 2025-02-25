3e2df52c9d42611a2b56be19807993db2ac32dcc
MThVisStud Coll <MORGANTHOMAS2@exe-coll.ac.uk>
2025-02-25 14:04:39 +0000

    Added simple routes for login/logout/create acc testing, and session storing.

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import requests as req
 import json
 from argparse import ArgumentParser
 
 app = Flask(__name__)
+
+app.secret_key = "THISISASECRETKEY"
 
 @app.route("/")
 def index():
@@ -12,25 +14,28 @@ def index():
 
 API_HOST = "http://52.156.24.253"  # Replace with your actual API hostname
 
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('user_id', default=None)
+
+    return render_template('login_signup.html', resp="")
 
 @app.route('/user', methods=['POST'])
 def create_user():
     # Get data from the request
-    parser = ArgumentParser
-    parser.add_argument("email", type=str)
-    parser.add_argument("password", type=str)
-    parser.add_argument("firstname", type=str)
-    parser.add_argument("lastname", type=str)
-    parser.add_argument("usertype", type=str)
 
-    data = parser.parse_args()
-
-    print(data['firstname'])
+    data = {
+        "email": request.form.get("email"),
+        "password": request.form.get("password"),
+        "firstname": request.form.get("firstname"),
+        "lastname": request.form.get("lastname"),
+        "usertype": request.form.get("usertype")
+    }
 
     # Send data to the API
     response = req.post(f"{API_HOST}/user", json=data)
 
-    data = json.loads(response)
+    data = response.json()
 
     if data["response"] == 200:
         return render_template("login_signup.html", resp="User created!")
@@ -41,13 +46,20 @@ def create_user():
 @app.route('/auth', methods=['POST'])
 def login_user():
     # Get login data from request
-    data = request.json
+    data = {
+        "email": request.form.get("email"),
+        "password": request.form.get("password")
+    }
+
     # Send login request to the API
     response = req.post(f"{API_HOST}/auth", json=data)
 
-    data = json.loads(response)
+    data = response.json()
+
 
     if data["response"] == 200:
+
+        session["user_id"] = data["data"]["id"]
         return render_template("login_signup.html", resp=f"Logged in! User:{data['data']['id']}")
     else:
         return render_template("login_signup.html", resp=f"User not logged in\nMessage:{data['message']}")
