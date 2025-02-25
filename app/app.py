08ef120853dd81be4923003450e39b08de6c1a4e
Home Computer (Morgan) <morgan7pl@gmail.com>
2025-02-25 06:27:40 +0000

    Test API with login/signup

--- app/app.py
+++ app/app.py
@@ -1,10 +1,46 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests as req
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="index")
+    return render_template('login_signup.html')
+
+API_HOST = "http://52.156.24.253"  # Replace with your actual API hostname
+
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    # Get data from the request
+    data = request.json
+    # Send data to the API
+    response = req.post(f"{API_HOST}/user", json=data)
+
+    data = json.loads(response)
+
+    if data["response"] == 200:
+        return "User created!"
+    else:
+        return "Failed to make user!"
+
+
+@app.route('/auth', methods=['POST'])
+def login_user():
+    # Get login data from request
+    data = request.json
+    # Send login request to the API
+    response = req.post(f"{API_HOST}/auth", json=data)
+
+    if data["response"] == 200:
+        return "Successful login"
+    else:
+        return "Failed login"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 app.run(debug=True)
